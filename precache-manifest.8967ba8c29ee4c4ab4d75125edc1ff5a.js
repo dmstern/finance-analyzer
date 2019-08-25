c8bea52a295ba937ac384be7f43d28b7b004bd74
Daniel Morgenstern <morgenstern.daniel@gmail.com>
2019-08-25 12:15:21 +0200

    Updates

---
+++ precache-manifest.8967ba8c29ee4c4ab4d75125edc1ff5a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "200e36b3b3293e043921",
+    "url": "/finance-analyzer/static/js/runtime~main.200e36b3.js"
+  },
+  {
+    "revision": "bd23f5af1d8293be1723",
+    "url": "/finance-analyzer/static/js/main.bd23f5af.chunk.js"
+  },
+  {
+    "revision": "5e3d25b64e142426d8f7",
+    "url": "/finance-analyzer/static/js/1.5e3d25b6.chunk.js"
+  },
+  {
+    "revision": "bd23f5af1d8293be1723",
+    "url": "/finance-analyzer/static/css/main.1e51f8b0.chunk.css"
+  },
+  {
+    "revision": "3dc07c71872f4192199e54be7f46fd9f",
+    "url": "/finance-analyzer/index.html"
+  }
+];
